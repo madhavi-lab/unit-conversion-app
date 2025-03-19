8c88163c91777ab4690bf7b040c00bcd422946ec
Madhavi <vmadhaviv@gmail.com>
2025-03-19 20:49:08 +0800

    Initial code commit

---
+++ src/data/units.js
@@ -0,0 +1,19 @@
+export const UNITS = [
+  {
+    name: "length",
+    icon: require("../assets/length.png"),
+    metrics: [
+      { id: "cm", name: "cm", order: 1 },
+      { id: "m", name: "m", order: 2 },
+    ],
+  },
+  {
+    name: "weight",
+    icon: require("../assets/weight.png"),
+    metrics: [
+      { id: "mg", name: "mg", order: 1 },
+      { id: "g", name: "g", order: 2 },
+      { id: "kg", name: "kg", order: 3 },
+    ],
+  },
+];
